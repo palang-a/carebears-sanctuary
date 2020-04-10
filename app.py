15f766ffb39dd6fc9672e1822e69e6d036d175ad
palang-a <palang.anivan@capgemini.com>
2020-04-10 19:19:48 +1000

    update the app

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
-def hello():
-    return "Hello World"
+def home():
+    return render_template('home.html')
 
 @app.route("/read")
-    return "Read"
+def read():
+    return render_template("read.html", messages="WIP TEXT")
 
 @app.route("/write")
 def write():
-    return "Write"
+    return render_template("write.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
