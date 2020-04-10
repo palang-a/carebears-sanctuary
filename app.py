4782c8183d96473e9f3c9abf736fdb338b8fd1f0
palang-a <palang.anivan@capgemini.com>
2020-04-10 15:47:10 +1000

    added html page

--- app.py
+++ app.py
@@ -2,5 +2,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def hello():
     return "Hello World"
+
+@app.route("/read")
+    return "Read"
+
+@app.route("/write")
+def write():
+    return "Write"
+
+if __name__ == '__main__':
+    app.run(debug=True)
